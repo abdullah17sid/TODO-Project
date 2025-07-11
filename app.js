8a49e90c966bda196e7ba631e9305ffd2b4f7c8b
Abdullah siddiqui <abdullahsmsiddiqui@gmail.com>
2025-07-11 07:57:41 +0530

    Initial commit of Todo List project

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb://localhost:27017/todoo");
+
+const taskSchema = new mongoose.Schema({
+  name: String
+});
+
+const Task = mongoose.model("Task", taskSchema);
+
+app.get("/", function(req, res) {
+  Task.find({}, function(err, tasks) {
+    if (!err) {
+      res.render("list", { ejes: tasks });
+    } else {
+      res.send("Error loading tasks");
+    }
+  });
+});
+
+app.post("/", function(req, res) {
+  const newTask = new Task({ name: req.body.ele1 });
+  newTask.save(function(err) {
+    res.redirect("/");
+  });
+});
+
+app.post("/edit", function(req, res) {
+  const editId = req.body.editId;
+  const updatedText = req.body.updatedText;
+  Task.findByIdAndUpdate(editId, { name: updatedText }, function(err) {
+    res.redirect("/");
+  });
+});
+
+app.post("/delete", function(req, res) {
+  const deleteId = req.body.deleteId;
+  Task.findByIdAndDelete(deleteId, function(err) {
+    res.redirect("/");
+  });
+});
+
+app.listen(5000, function() {
+  console.log("Server started on port 5000");
+});
